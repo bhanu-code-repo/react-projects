1db15a5a4dcb0959c1ba445f72bc61b99ba7fb3a
Bhanu <bpst.web@gmail.com>
2023-11-01 22:01:59 +0000

    project code

---
+++ 04-tailwind-props/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <div className="bg-green-300 text-black p-4 rounded-xl font-bold mb-4">
+        Learn Tailwind
+      </div>
+      <Card />
+      <Card />
+    </>
+  );
+}
+
+export default App;
